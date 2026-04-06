85e19bc07cdcd7a7d5bac5b5279f2a65a9dab36c
Ashish Lukka <ashishlukka2005@gmail.com>
2026-04-06 19:45:58 +0530

    fix: deployment issues

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use(async (req, res, next) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Server is running' });
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/financial-records', financialRecordRoutes);
